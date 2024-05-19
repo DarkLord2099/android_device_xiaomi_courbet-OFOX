05f1b35c85bbb0aa750318b2904e640adec688cb
Abdalrahman Eissa <abdalrahmaneissa@gmail.com>
2024-05-19 23:57:35 +0300

    Add Device mk

---
+++ device.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+
