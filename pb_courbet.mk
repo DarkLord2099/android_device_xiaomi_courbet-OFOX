6e673d98699b0f1d7564ed03d5e4b0bdc8f096cb
Abdalrahman Eissa <abdalrahmaneissa@gmail.com>
2024-05-20 00:12:14 +0300

    Add Device Products

--- pb_courbet.mk
+++ pb_courbet.mk
@@ -13,3 +13,17 @@ $(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from courbet device
 $(call inherit-product, device/xiaomi/courbet/device.mk)
+
+# Products For Device
+PRODUCT_DEVICE := courbet
+PRODUCT_NAME := pb_courbet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 11 Lite
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="courbet_global-user 13 RKQ1.210614.002 V14.0.3.0.TKQMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/courbet_global/courbet:13/RKQ1.210614.002/V14.0.3.0.TKQMIXM:user/release-keys
