c95faf2ce74520fd1a09a995765169142e016f9b
Abdalrahman Eissa <abdalrahmaneissa@gmail.com>
2024-05-20 00:11:43 +0300

    Add Device Inherit Product

--- pb_courbet.mk
+++ pb_courbet.mk
@@ -7,3 +7,9 @@
 # Release name
 PRODUCT_RELEASE_NAME := courbet
 DEVICE_PATH := device/xiaomi/courbet
+
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit from courbet device
+$(call inherit-product, device/xiaomi/courbet/device.mk)
