51530ef4ae8df9b7b91db0b0e21cf557542615f6
Abdalrahman Eissa <abdalrahmaneissaa@gmail.com>
2024-07-04 10:03:34 +0300

    Update PBRB dependencies

--- pb_courbet.mk
+++ pb_courbet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,9 +20,9 @@ PRODUCT_NAME := pb_courbet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi 11 Lite
 PRODUCT_MANUFACTURER := xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Device Fingerprint
 BUILD_FINGERPRINT := Xiaomi/courbet_global/courbet:13/TKQ1.221013.002/V14.0.4.0.TKQMIXM:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
